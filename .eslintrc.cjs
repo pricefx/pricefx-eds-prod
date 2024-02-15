9d18a9626758c841f9be5022078e1503efc04a14
Steven Carter <steven.carter@bounteous.com>
2024-02-15 15:00:46 -0500

    feat: adding commitlint, husky, lint-staged, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: ["airbnb-base", "prettier"],
+  env: {
+    browser: true,
+  },
+  plugins: ["prettier"],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    // allow reassigning param
+    "prettier/prettier": "error",
+    "no-param-reassign": [2, { props: false }],
+    "linebreak-style": ["error", "unix"],
+    "import/extensions": [
+      "error",
+      {
+        js: "always",
+      },
+    ],
+  },
+};
