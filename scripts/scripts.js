801623c3510ee7acf59eef86fc25ceaf2a197acb
Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>
2024-04-11 10:55:30 +0530

    feat: Embed video popup & other Style updates (#31)
    * feat: embed video popup updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,6 +29,22 @@ async function loadFonts() {
 }
 
 /**
+ * links to a /modals/ path  are automatically transformed into a modal.
+ * @param {Element} element
+ */
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/components/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal({ fragmentUrl: origin.href });
+    }
+  });
+}
+
+/**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
@@ -84,6 +100,8 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc);
+
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
