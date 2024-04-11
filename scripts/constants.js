7bbd8364e350066196077acb150aca0d8d469129
Swathi Raju <109958175+SwathiPrasadRaju@users.noreply.github.com>
2024-04-11 15:40:11 +0530

    Feature/pai 42 fe breadcrumbs (#33)
    * feat: breadcrumb js & style updates
    
    ---------
    
    Co-authored-by: Muneeskumar Rajendran <159912467+muneeskumar-rajendran@users.noreply.github.com>

--- scripts/constants.js
+++ scripts/constants.js
@@ -10,4 +10,10 @@ const LINKEDIN =
 const EMAIL =
   '<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="34" height="34" viewBox="0 0 1920 1920"><path d="M0 1694.235h1920V226H0v1468.235ZM112.941 376.664V338.94H1807.06v37.723L960 1111.233l-847.059-734.57ZM1807.06 526.198v950.513l-351.134-438.89-88.32 70.475 378.353 472.998H174.042l378.353-472.998-88.32-70.475-351.134 438.89V526.198L960 1260.768l847.059-734.57Z" fill-rule="evenodd"/></svg>';
 
-export { FACEBOOK, TWITTER, LINKEDIN, EMAIL };
+const HOME =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none"><path d="M3 12.5L5 10.5M5 10.5L12 3.5L19 10.5M5 10.5V20.5C5 21.0523 5.44772 21.5 6 21.5H9M19 10.5L21 12.5M19 10.5V20.5C19 21.0523 18.5523 21.5 18 21.5H15M9 21.5C9.55228 21.5 10 21.0523 10 20.5V16.5C10 15.9477 10.4477 15.5 11 15.5H13C13.5523 15.5 14 15.9477 14 16.5V20.5C14 21.0523 14.4477 21.5 15 21.5M9 21.5H15" stroke="#484D56" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+
+const RIGHTARROW =
+  '<span class="breadcrumb-separator"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none"><path d="M14 5.5L21 12.5M21 12.5L14 19.5M21 12.5L3 12.5" stroke="#484D56" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></span>';
+
+export { FACEBOOK, TWITTER, LINKEDIN, EMAIL, HOME, RIGHTARROW };
