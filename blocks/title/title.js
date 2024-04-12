ff5659e882b61072d8da87a39dddcd7c527dd1ae
Swathi <swathi.raju@bounteous.com>
2024-04-12 17:16:24 +0530

    feat: rte title component changes

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -1,6 +1,11 @@
 export default async function decorate(block) {
-  const [titleData] = block.children;
-  block.textContent = '';
-  block.classList.add(titleData.textContent.trim());
-  block.appendChild(block.textContent);
+  const titleData = block.querySelector("div[data-aue-prop='title']")?.textContent.trim() || '';
+  const titleType = block.querySelector("div[data-aue-prop='type']")?.textContent.trim() || 'h2';
+  block.innerHTML = '';
+  if (titleData) {
+    const titleTypeElement = document.createElement(titleType);
+    titleTypeElement.classList.add(titleType);
+    titleTypeElement.innerHTML = titleData;
+    block.append(titleTypeElement);
+  }
 }
