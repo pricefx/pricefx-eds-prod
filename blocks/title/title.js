be1fc3d4315be0911fce1d01aa94d1c78217d368
Swathi <swathi.raju@bounteous.com>
2024-04-14 13:44:52 +0530

    feat: title component style changes

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -2,12 +2,10 @@ export default async function decorate(block) {
   const titleData = block.querySelector("div[data-aue-prop='title']")?.textContent.trim() || '';
   const titleType = block.querySelector("div[data-aue-prop='type']")?.textContent.trim() || 'h2';
   const titleColor = block.querySelector("div[data-aue-prop='fontColor']")?.textContent.trim() || '';
-  const titleMargin = block.querySelector("div[data-aue-prop='margin']")?.textContent.trim() || '';
   block.innerHTML = '';
   if (titleData) {
     const titleElement = document.createElement(titleType);
     block.classList.add(titleColor);
-    block.classList.add(titleMargin);
     titleElement.innerHTML = titleData;
     block.append(titleElement);
   }
