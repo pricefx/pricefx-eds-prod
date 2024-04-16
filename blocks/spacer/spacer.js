be7fa164bd4cf5d3930509285c5c825b8210ebd2
Swathi Raju <109958175+SwathiPrasadRaju@users.noreply.github.com>
2024-04-16 11:50:17 +0530

    Feature/pai 42 fe breadcrumbs issue fix (#48)
    * feat: breadcrumb style updates

--- blocks/spacer/spacer.js
+++ blocks/spacer/spacer.js
@@ -1,6 +1,4 @@
 export default async function decorate(block) {
-  const [spacerData] = block.children;
   block.textContent = '';
-  block.classList.add(spacerData.textContent.trim());
   block.appendChild(block.textContent);
 }
