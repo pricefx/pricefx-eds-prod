98e4939f5c6aef115dd4821bcc3fd398ece59c41
Muneeskumar Rajendran <muneeskumar.rajendran@bounteous.com>
2024-04-25 19:17:49 +0530

    Revert "Merge pull request #83 from pricefx/feature/pai-132-article-metadata-block"
    This reverts commit 7431dc55f6ad87a9d8efa9dee387fd0a9727b6ae, reversing
    changes made to 3e3e957ce8f33f98b586bc9c422278c25a17f95b.

--- scripts/url-constants.js
+++
@@ -1,6 +0,0 @@
-// Define base paths for URLs
-const BASE_CONTENT_PATH = '/content/pricefx/en'; // En base content path
-const BASE_STYLE_GUIDE_PATH = '/content/pricefx/style-guide'; // Style guide base content path
-
-// Export the constants
-export { BASE_CONTENT_PATH, BASE_STYLE_GUIDE_PATH };
