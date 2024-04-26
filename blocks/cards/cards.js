8bd598b1bc38337d9f6a4f8f2d2ea728e35d734b
Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>
2024-04-26 16:20:54 +0530

    Feature/pai 51 card component updates (#97)
    * feat: pai 51 fixing error in Cards js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -26,32 +26,28 @@ function generateCardDom(props) {
   // Build DOM
   if (isClickable?.textContent.trim() === 'true') {
     const link = cta.querySelector('a');
-    const cardDOM = document.createRange().createContextualFragment(`
-      <li>
+    const cardDOM = `
         <a class="cards-card-link" href="${link ? link.href : '#'}" target="${ctaTarget.textContent.trim() === 'true' ? '_blank' : ''}">
           <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
           <div class='cards-card-body'>
               ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
               ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
-              ${description?.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
+              ${description?.children.length > 0 ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
               <div class='cards-card-cta'>${decorateCTA(cta, ctaLabel, ctaTarget, isClickable).outerHTML}</div>
           </div>
         </a>
-      </li>
-    `);
+    `;
     return cardDOM;
   }
-  const cardDOM = document.createRange().createContextualFragment(`
-      <li>
+  const cardDOM = `
         <div class='cards-card-image'>${picture ? picture.outerHTML : ''}</div>
         <div class='cards-card-body'>
             ${eyebrow?.textContent.trim() !== '' ? `<div class='cards-card-eyebrow'>${eyebrow.textContent.trim().toUpperCase()}</div>` : ``}
             ${title?.children.length > 0 ? `<div class='cards-card-title'><h6>${title.textContent.trim()}</h6></div>` : ``}
-            ${description?.children.length > 0 !== '' ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
+            ${description?.children.length > 0 ? `<div class='cards-card-description'>${description.innerHTML}</div>` : ``}
             <div class='cards-card-cta'>${decorateCTA(cta, ctaLabel, ctaTarget).outerHTML}</div>
         </div>
-      </li>
-    `);
+    `;
   return cardDOM;
 }
 
@@ -59,8 +55,12 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row, index) => {
+    const li = document.createElement('li');
+    [...row.attributes].forEach(({ nodeName, nodeValue }) => {
+      li.setAttribute(nodeName, nodeValue);
+    });
     // Adding Style options
-    if (index < 3 && row.matches('div')) {
+    if (index < 3) {
       if (row.textContent.trim()) {
         let className = '';
         if (index === 2) {
@@ -75,8 +75,8 @@ export default function decorate(block) {
       }
       return;
     }
-
-    ul.append(generateCardDom(row.children));
+    li.innerHTML = generateCardDom(row.children);
+    ul.append(li);
   });
 
   ul.querySelectorAll('img').forEach((img) =>
