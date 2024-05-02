c7caf029306eeca7bf5c2c86b3ff4c5590c03a33
Nandhiniraghu <nandhini.raghu@bounteous.com>
2024-05-02 10:49:12 +0530

    feat: iframe-js-removed

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,27 +1,4 @@
 export default function decorate(block) {
-  const [urlElement, widthElement, heightElement] = block.children;
   const container = document.createElement('div');
-  block.textContent = '';
-  container.classList.add('iframe', 'container');
-  if (urlElement) {
-    const url = new URL(urlElement.textContent);
-    const iframe = document.createElement('iframe');
-    container.appendChild(iframe);
-    iframe.src = url.href;
-  }
-
-  if (heightElement) {
-    const height = Number(heightElement.textContent);
-    container.classList.remove('noheight');
-    container.style.height = `${height}px`;
-  } else {
-    container.classList.add('noheight');
-  }
-
-  if (widthElement) {
-    const width = Number(widthElement.textContent);
-    container.style.width = `${width}px`;
-  }
-
   block.appendChild(container);
 }
