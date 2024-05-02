87366a86caced1c60d583f84d6dbaa8da76b8e77
Nandhiniraghu <nandhini.raghu@bounteous.com>
2024-05-02 10:36:28 +0530

    feat: iframe-js-modified

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,24 +1,25 @@
 export default function decorate(block) {
   const [urlElement, widthElement, heightElement] = block.children;
-  const height = Number(heightElement.textContent);
-  const width = Number(widthElement.textContent);
-  const url = new URL(urlElement.textContent);
   const container = document.createElement('div');
-  const iframe = document.createElement('iframe');
-
   block.textContent = '';
   container.classList.add('iframe', 'container');
-  container.appendChild(iframe);
-  iframe.src = url.href;
+  if (urlElement) {
+    const url = new URL(urlElement.textContent);
+    const iframe = document.createElement('iframe');
+    container.appendChild(iframe);
+    iframe.src = url.href;
+  }
 
-  if (height) {
+  if (heightElement) {
+    const height = Number(heightElement.textContent);
     container.classList.remove('noheight');
     container.style.height = `${height}px`;
   } else {
     container.classList.add('noheight');
   }
 
-  if (width) {
+  if (widthElement) {
+    const width = Number(widthElement.textContent);
     container.style.width = `${width}px`;
   }
 
