52d4fb19a064e85552a9b3e691611b6f1f63dab9
Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>
2024-05-10 13:08:46 +0530

    Feature/pai 125 image linkable column (#178)
    * feat: pai 125 linkpfx js fix to accommodate in columns

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -11,8 +11,25 @@ export default function decorate(block) {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
+        const picParent = pic.parentElement;
         if (picWrapper) {
           picWrapper.classList.add('columns-img-col');
+        }
+
+        const linkwrapper = pic.parentElement.nextElementSibling;
+        const aInsideStrong = linkwrapper?.querySelector('strong a');
+        const aInsideEM = linkwrapper?.querySelector('em a');
+        const a = linkwrapper?.querySelector('a');
+        const isTarget = linkwrapper?.nextElementSibling;
+        if (a && !aInsideStrong && !aInsideEM) {
+          a.textContent = '';
+          a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
+          a.append(pic);
+          picParent.append(a);
+          linkwrapper.remove();
+          if (isTarget) {
+            isTarget.remove();
+          }
         }
       }
     });
