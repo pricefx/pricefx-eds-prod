66e8f4618c5f9dd5da82d87cc45bace3858e62f0
Swathi Raju <109958175+SwathiPrasadRaju@users.noreply.github.com>
2024-05-13 11:16:12 +0530

    Feature/pai 140 fe brand (#186)
    * feat: brand page UI
    
    ---------
    
    Co-authored-by: Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,5 @@
 import { environmentMode } from '../../scripts/global-functions.js';
+import { decorateIcons } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
@@ -7,11 +8,21 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
-      if (col.textContent.trim() === '') {
+      if (col.firstElementChild === null) {
         col.style.border = 'none';
         col.style.background = 'none';
         col.style.boxShadow = 'none';
       }
+
+      const downloadLink = col.querySelector('a');
+      if (downloadLink?.textContent === 'download') {
+        downloadLink.classList.add('download-btn');
+        downloadLink.setAttribute('aria-label', 'download');
+        const downloadImg = `<span class="icon icon-download"></span>`;
+        downloadLink.innerHTML = downloadImg;
+        decorateIcons(downloadLink, '', 'Pricefx', 'png');
+      }
+
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
@@ -25,7 +36,7 @@ export default function decorate(block) {
         const aInsideEM = linkwrapper?.querySelector('em a');
         const a = linkwrapper?.querySelector('a');
         const isTarget = linkwrapper?.nextElementSibling;
-        if (a && !aInsideStrong && !aInsideEM) {
+        if (a && !aInsideStrong && !aInsideEM && !a.classList.contains('download-btn')) {
           a.textContent = '';
           a.target = isTarget?.textContent.trim() === 'true' ? '_blank' : '';
           a.append(pic);
