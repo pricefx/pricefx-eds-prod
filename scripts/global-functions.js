e08cb2bcdad069d561d7ffd5bd754f8c72523093
Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>
2024-05-14 19:58:21 +0530

    feat: pai 68 related articles js and ui fixes (#201)
    * feat: pai 68 related articles js and ui fixes
    
    ---------
    
    Co-authored-by: Muneeskumar Rajendran <159912467+muneeskumar-rajendran@users.noreply.github.com>

--- scripts/global-functions.js
+++ scripts/global-functions.js
@@ -8,5 +8,16 @@ function environmentMode() {
   }
   return 'publish';
 }
+
+/**
+ * Function to return the formatted date by passing the Timestamp.
+ * @returns {string} Formatted  Date string (September 22, 2024)
+ */
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { month: 'long', day: 'numeric', year: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { environmentMode };
+export { environmentMode, formatDate };
