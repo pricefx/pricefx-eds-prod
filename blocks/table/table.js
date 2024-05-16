7c3a4a6d4464d5436520369282e3e9e0a90aed83
archanaganesan11 <165906746+archanaganesan11@users.noreply.github.com>
2024-05-16 19:04:24 +0530

    Bugfix/pai 57 fe table (#223)
    * feat: table changes
    
    Co-authored-by: Vaitheeswaran Nagalingam <vaitheeswaran.nagalingam@bounteous.com>

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -19,7 +19,7 @@ export default async function decorate(block) {
 
   /* Table component  */
   const table = document.createElement('table');
-  const variation = tableVariation.textContent.replace(/\s+/g, ' ');
+  const variation = tableVariation.textContent.trim();
 
   rows.forEach((rowDiv, rowIndex) => {
     const row = document.createElement('tr');
