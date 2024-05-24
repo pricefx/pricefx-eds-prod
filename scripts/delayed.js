f00c332c274499c589ab1a5ad608cb80a9471c94
Si-Jia Mao <70045188+iSupercell@users.noreply.github.com>
2024-05-24 00:17:30 -0400

    Feature/PAI-59 FE Marketo Form (#261)
    * feat: pai-59 added logic to load and render marketo form on page
    
    ---------
    
    Co-authored-by: Swathi Raju <109958175+SwathiPrasadRaju@users.noreply.github.com>
    Co-authored-by: Vaitheeswaran Nagalingam <161038987+Vaitheesaran-nagalingam@users.noreply.github.com>

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,54 @@
 // eslint-disable-next-line import/no-cycle
+/* eslint-disable no-undef */
 import { sampleRUM } from './aem.js';
 import { environmentMode } from './global-functions.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+
+// MARKETO SANDBOX INTEGRATION SCRIPT
+(function () {
+  let didInit = false;
+  function initMunchkin() {
+    if (didInit === false) {
+      didInit = true;
+      Munchkin.init('542-QYC-710');
+    }
+  }
+  const s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//munchkin.marketo.net/munchkin.js';
+  s.onreadystatechange = function () {
+    if (this.readyState === 'complete' || this.readyState === 'loaded') {
+      initMunchkin();
+    }
+  };
+  s.onload = initMunchkin;
+  document.getElementsByTagName('head')[0].appendChild(s);
+})();
+
+// MARKETO PRODUCTION INTEGRATION SCRIPT
+(function () {
+  let didInit = false;
+  function initMunchkin() {
+    if (didInit === false) {
+      didInit = true;
+      Munchkin.init('289-DOX-852');
+    }
+  }
+  const s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//munchkin.marketo.net/munchkin-beta.js';
+  s.onreadystatechange = function () {
+    if (this.readyState === 'complete' || this.readyState === 'loaded') {
+      initMunchkin();
+    }
+  };
+  s.onload = initMunchkin;
+  document.getElementsByTagName('head')[0].appendChild(s);
+})();
 
 // add more delayed functionality here
 
