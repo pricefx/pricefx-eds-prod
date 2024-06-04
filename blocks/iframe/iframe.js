e8cbaca22c1a5da7d0dcc699f5d52d2f4d624b9c
Si-Jia Mao <sijia.mao@bounteous.com>
2024-06-04 19:04:09 -0400

    feat: pai-221 implemented fe iframe updates

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,26 +1,83 @@
+// Render badges based on authored field
+const renderBadges = (badges) => {
+  let markup = '';
+  badges.forEach((badge) => {
+    markup += `
+      <div class="badge__container">
+        <a title="${badge.textContent}" href="https://www.g2.com/products/pricefx/reviews?utm_source=rewards-badge">
+          <img decoding="async" alt="${badge.textContent}" data-src="${badge.href}" src="${badge.href}" class="lazyloaded badge__icon">
+        </a>
+      </div>
+    `;
+  });
+  return markup;
+};
+
+// Render iframes based on authored field
+const renderIframes = (iframes, height, width) => {
+  let markup = '';
+
+  if (iframes.length === 3) {
+    markup = `
+      <div class="iframe__left-column" ${width ? `style=max-width:${width + 36}px;` : ''}>
+        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
+          <iframe src="${iframes[0].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
+        </div>
+        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
+          <iframe src="${iframes[1].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
+        </div>
+      </div>
+      <div class="iframe__right-column" ${width ? `style=max-width:${width + 36}px;` : ''}>
+        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
+          <iframe src="${iframes[2].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height + height + 36}px;` : ''}></iframe>
+        </div>
+      </div>
+    `;
+  } else {
+    iframes.forEach((iframe) => {
+      const iframeSource = iframe.textContent.trim();
+      markup += `
+        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
+          <iframe src="${iframeSource}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
+        </div>
+      `;
+    });
+  }
+  return markup;
+};
+
 export default function decorate(block) {
-  const [urlElement, widthElement, heightElement] = block.children;
+  const [badgeLinks, iframeLinks, widthElement, heightElement] = block.children;
+  const badgeItems = badgeLinks.querySelectorAll('a');
+  const iframeItems = iframeLinks.querySelectorAll('p');
   const height = Number(heightElement.textContent);
   const width = Number(widthElement.textContent);
-  const url = new URL(urlElement.textContent);
-  const container = document.createElement('div');
-  const iframe = document.createElement('iframe');
-
   block.textContent = '';
-  container.classList.add('iframe', 'container');
-  container.appendChild(iframe);
-  iframe.src = url.href;
 
-  if (height) {
-    container.classList.remove('noheight');
-    container.style.height = `${height}px`;
-  } else {
-    container.classList.add('noheight');
+  // Create badge wrapper element and render individual badges
+  if (badgeLinks.textContent.trim() !== '') {
+    const badgeWrapper = document.createElement('div');
+    badgeWrapper.classList.add('badge__wrapper');
+    block.append(badgeWrapper);
+    badgeWrapper.innerHTML = renderBadges(badgeItems);
   }
 
-  if (width) {
-    container.style.width = `${width}px`;
-  }
+  // Create iFrame wrapper element and render individual iFrames
+  if (iframeLinks.textContent.trim() !== '') {
+    const iframeWrapper = document.createElement('div');
+    iframeWrapper.classList.add('iframe__wrapper');
 
-  block.appendChild(container);
+    // Add custom class for 3 iframes
+    if (iframeItems.length === 3) {
+      iframeWrapper.classList.add('iframe__wrapper--three-iframes');
+    }
+
+    // Add custom class if badges are present
+    if (badgeLinks.textContent.trim() !== '') {
+      iframeWrapper.classList.add('frame__wrapper--with-badge');
+    }
+
+    block.append(iframeWrapper);
+    iframeWrapper.innerHTML = renderIframes(iframeItems, height, width);
+  }
 }
