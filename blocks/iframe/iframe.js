fcfec77e8ab358ca97c65936b26c7a746f1e85bb
Si-Jia Mao <sijia.mao@bounteous.com>
2024-06-05 17:38:39 -0400

    feat: pai-221 updated method used to generate badges and iframe markup

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,49 +1,103 @@
 // Render badges based on authored field
 const renderBadges = (badges) => {
-  let markup = '';
+  const fragment = document.createDocumentFragment();
   badges.forEach((badge) => {
-    markup += `
-      <div class="badge__container">
-        <a title="${badge.textContent}" href="https://www.g2.com/products/pricefx/reviews?utm_source=rewards-badge">
-          <img decoding="async" alt="${badge.textContent}" data-src="${badge.href}" src="${badge.href}" class="lazyloaded badge__icon">
-        </a>
-      </div>
-    `;
+    const badgeContainer = document.createElement('div');
+    badgeContainer.classList.add('badge__container');
+    const badgeLink = document.createElement('a');
+    badgeLink.title = badge.textContent;
+    badgeLink.href = 'https://www.g2.com/products/pricefx/reviews?utm_source=rewards-badge';
+    const badgeImg = document.createElement('img');
+    badgeImg.decoding = 'async';
+    badgeImg.alt = badge.textContent;
+    badgeImg.dataset.src = badge.href;
+    badgeImg.src = badge.href;
+    badgeImg.loading = 'lazy'; // Add lazy loading for images
+    badgeImg.classList.add('badge__icon');
+    badgeLink.appendChild(badgeImg);
+    badgeContainer.appendChild(badgeLink);
+    fragment.appendChild(badgeContainer);
   });
-  return markup;
+  return fragment;
 };
 
 // Render iframes based on authored field
 const renderIframes = (iframes, height, width) => {
-  let markup = '';
-
+  const fragment = document.createDocumentFragment();
   if (iframes.length === 3) {
-    markup = `
-      <div class="iframe__left-column" ${width ? `style=max-width:${width + 36}px;` : ''}>
-        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
-          <iframe src="${iframes[0].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
-        </div>
-        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
-          <iframe src="${iframes[1].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
-        </div>
-      </div>
-      <div class="iframe__right-column" ${width ? `style=max-width:${width + 36}px;` : ''}>
-        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
-          <iframe src="${iframes[2].textContent.trim()}" frameborder="0" style=${height ? `min-height:${height + height + 36}px;` : ''}></iframe>
-        </div>
-      </div>
-    `;
+    // Creating left column and children elements
+    const leftColumn = document.createElement('div');
+    leftColumn.classList.add('iframe__left-column');
+    if (width) {
+      leftColumn.setAttribute('style', `max-width:${width + 36}px;`);
+    }
+    const iframeContainerOne = document.createElement('div');
+    iframeContainerOne.classList.add('iframe__container');
+    if (width) {
+      iframeContainerOne.setAttribute('style', `max-width:${width + 36}px;`);
+    }
+    const iframeElOne = document.createElement('iframe');
+    iframeElOne.src = iframes[0].textContent.trim();
+    iframeElOne.setAttribute('frameborder', '0');
+    if (height) {
+      iframeElOne.setAttribute('style', `min-height:${height}px;`);
+    }
+    iframeContainerOne.appendChild(iframeElOne);
+    leftColumn.appendChild(iframeContainerOne);
+    const iframeContainerTwo = document.createElement('div');
+    iframeContainerTwo.classList.add('iframe__container');
+    if (width) {
+      iframeContainerTwo.setAttribute('style', `max-width:${width + 36}px;`);
+    }
+    const iframeElTwo = document.createElement('iframe');
+    iframeElTwo.src = iframes[1].textContent.trim();
+    iframeElTwo.setAttribute('frameborder', '0');
+    if (height) {
+      iframeElTwo.setAttribute('style', `min-height:${height}px;`);
+    }
+    iframeContainerTwo.appendChild(iframeElTwo);
+    leftColumn.appendChild(iframeContainerTwo);
+    fragment.appendChild(leftColumn);
+
+    // Creating right column and children elements
+    const rightColumn = document.createElement('div');
+    rightColumn.classList.add('iframe__right-column');
+    if (width) {
+      rightColumn.setAttribute('style', `max-width:${width + 36}px;`);
+    }
+    const iframeContainerThree = document.createElement('div');
+    iframeContainerThree.classList.add('iframe__container');
+    if (width) {
+      iframeContainerThree.setAttribute('style', `max-width:${width + 36}px;`);
+    }
+    const iframeElThree = document.createElement('iframe');
+    iframeElThree.src = iframes[2].textContent.trim();
+    iframeElThree.setAttribute('frameborder', '0');
+    if (height) {
+      iframeElThree.setAttribute('style', `min-height:${height * 2 + 36}px;`);
+    }
+    iframeContainerThree.appendChild(iframeElThree);
+    rightColumn.appendChild(iframeContainerThree);
+    fragment.appendChild(rightColumn);
   } else {
     iframes.forEach((iframe) => {
       const iframeSource = iframe.textContent.trim();
-      markup += `
-        <div class="iframe__container" ${width ? `style=max-width:${width + 36}px;` : ''}>
-          <iframe src="${iframeSource}" frameborder="0" style=${height ? `min-height:${height}px;` : ''}></iframe>
-        </div>
-      `;
+      const iframeContainer = document.createElement('div');
+      iframeContainer.classList.add('iframe__container');
+      if (width) {
+        iframeContainer.setAttribute('style', `max-width:${width + 36}px;`);
+      }
+      const iframeEl = document.createElement('iframe');
+      iframeEl.src = iframeSource;
+      iframeEl.setAttribute('frameborder', '0');
+      if (height) {
+        iframeEl.setAttribute('style', `min-height:${height}px;`);
+      }
+      iframeContainer.appendChild(iframeEl);
+      fragment.appendChild(iframeContainer);
     });
   }
-  return markup;
+  return fragment;
 };
 
 export default function decorate(block) {
@@ -56,17 +110,14 @@ export default function decorate(block) {
 
   // Create badge wrapper element and render individual badges
   if (badgeLinks.textContent.trim() !== '') {
-    const badgeFragment = new DocumentFragment();
     const badgeWrapper = document.createElement('div');
     badgeWrapper.classList.add('badge__wrapper');
-    badgeWrapper.innerHTML = renderBadges(badgeItems);
-    badgeFragment.append(badgeWrapper);
-    block.append(badgeFragment);
+    badgeWrapper.append(renderBadges(badgeItems));
+    block.append(badgeWrapper);
   }
 
   // Create iFrame wrapper element and render individual iFrames
   if (iframeLinks.textContent.trim() !== '') {
-    const iframeFragment = new DocumentFragment();
     const iframeWrapper = document.createElement('div');
     iframeWrapper.classList.add('iframe__wrapper');
 
@@ -80,8 +131,7 @@ export default function decorate(block) {
       iframeWrapper.classList.add('frame__wrapper--with-badge');
     }
 
-    iframeWrapper.innerHTML = renderIframes(iframeItems, height, width);
-    iframeFragment.append(iframeWrapper);
-    block.append(iframeFragment);
+    iframeWrapper.append(renderIframes(iframeItems, height, width));
+    block.append(iframeWrapper);
   }
 }
